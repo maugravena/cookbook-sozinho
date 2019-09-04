96967fbc87e08daac5ad9e32875d3ee5e63d46ee
Mauricio <maugravena@gmail.com>
2019-09-03 23:38:10 -0300

    user can logout

--- spec/feature/user_authenticates_to_the_site_spec.rb
+++ spec/feature/user_authenticates_to_the_site_spec.rb
@@ -12,4 +12,17 @@ feature 'User authenticates to the site' do
 
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
+
+  scenario 'and logout' do
+    visit root_path
+    click_on 'Entrar'
+
+    fill_in 'Email', with: 'email@email.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+
+    expect(page).to have_no_css('a', text: 'Entrar')
+    expect(page).to have_css('a', text: 'Sair')
+  end
 end
