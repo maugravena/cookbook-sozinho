a1ff61421805a25db92e150a5f00eb7f686a1bb1
Mauricio <maugravena@gmail.com>
2019-09-23 14:47:43 -0300

    asdf tool-version and email faker in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-User.create(email: 'user@email.com', password: '123123')
+User.create(email: Faker::Internet.email, password: '123123')
 RecipeType.create(name: 'Salgada')
 Cuisine.create(name: 'Brasileira')
 
